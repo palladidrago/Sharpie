5ff94ecee379521c68dbb60f8a93ba9d0512380d
Max Power <palladidrago@gmail.com>
2020-11-19 20:18:44 +0200

    Add mashov_api

--- sharpie_app/lib/main.dart
+++ sharpie_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sharpie_app/screens/intro/splash.dart';
 import 'package:sharpie_app/services/preferences.dart';
-
+import 'package:sharpie_app/services/helpers.dart';
 void main() async {
   runApp(MyApp());
   //Added check to main to see if the isFirst() function works. This returns the text "Instance of Future<bool>"
@@ -9,6 +9,8 @@ void main() async {
   final _prefs = Preferences.instance;
   bool iF = await _prefs.isFirst();
   print(iF);
+  var school = await MashovHelper.Test();
+  print(school.name);
 }
 
 class MyApp extends StatelessWidget {
--- sharpie_app/lib/services/helpers.dart
+++ sharpie_app/lib/services/helpers.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:mashov_api/mashov_api.dart';
 //Helps you with screen sizes
 
-Size displaySize(BuildContext context) {
-  //Returns the size of the display
-  return MediaQuery.of(context).size;
+class DisplaySize{
+  static Size displaySize(BuildContext context) {
+    //Returns the size of the display
+    return MediaQuery.of(context).size;
+  }
+
+  static double displayHeight(BuildContext context) {
+    //Returns height of the display
+    return displaySize(context).height;
+  }
+
+  static double displayWidth(BuildContext context) {
+    //Returns width of the display
+    return displaySize(context).width;
+  }
 }
 
-double displayHeight(BuildContext context) {
-  //Returns height of the display
-  return displaySize(context).height;
-}
+class MashovHelper{
 
-double displayWidth(BuildContext context) {
-  //Returns width of the display
-  return displaySize(context).width;
+  static Future<dynamic> Test() async{
+    var controller = MashovApi.getController();
+    var schoolsResult = await controller.getSchools();
+    return schoolsResult.value[10];
+  }
 }
