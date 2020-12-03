25e4f864b022fa94fec9aa423818da7a9c83c213
Max Power <palladidrago@gmail.com>
2020-12-03 22:51:32 +0200

    Fixed mashov test

--- sharpie_app/lib/main.dart
+++ sharpie_app/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:sharpie_app/screens/intro/splash.dart';
+import 'package:simple_mashovapi/simple_mashovapi.dart';
 void main() async {
   runApp(MyApp());
+  var controller = Controller();
+  await controller.login("", "" , "540484", "2021");
+  var grades = await controller.getGradeList(); //Returns list of Grades
+  print(grades[grades.length-1].id.toString()+ 'grade:' +grades[grades.length-1].grade.toString());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Splash();
+  Widget build(BuildContext context)  => Splash();
 }
