8fa8232a62882b698a73068188b57692791da9c2
Max Power <palladidrago@gmail.com>
2020-12-03 23:02:12 +0200

    Add dotenv

--- sharpie_app/lib/main.dart
+++ sharpie_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:sharpie_app/screens/intro/splash.dart';
 import 'package:simple_mashovapi/simple_mashovapi.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   runApp(MyApp());
   var controller = Controller();
-  await controller.login("", "" , "540484", "2021");
+  await DotEnv().load('.env');
+  await controller.login(DotEnv().env['USERNAME'],DotEnv().env['PASSWORD'], "540484", "2021");
   var grades = await controller.getGradeList(); //Returns list of Grades
-  print(grades[grades.length-1].id.toString()+ 'grade:' +grades[grades.length-1].grade.toString());
+  print(grades[grades.length-1].id.toString() + ' grade:' +grades[grades.length-1].grade.toString());
 }
 
 class MyApp extends StatelessWidget {
