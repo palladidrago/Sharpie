e8e047eb31e76ef3e2049a75940da118c0f937c2
David Ohayon <davideytanohayon@gmail.com>
2020-12-03 23:13:39 +0200

    Added my dotenv

--- sharpie_app/lib/main.dart
+++ sharpie_app/lib/main.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:sharpie_app/screens/intro/splash.dart';
 import 'package:simple_mashovapi/simple_mashovapi.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 void main() async {
   runApp(MyApp());
   var controller = Controller();
   await DotEnv().load('.env');
-  await controller.login(DotEnv().env['USERNAME'],DotEnv().env['PASSWORD'], "540484", "2021");
+  await controller.login(
+      DotEnv().env['USERNAME'], DotEnv().env['PASSWORD'], "540484", "2021");
   var grades = await controller.getGradeList(); //Returns list of Grades
-  print(grades[grades.length-1].id.toString() + ' grade:' +grades[grades.length-1].grade.toString());
+  for (var i = 0; i < grades.length; i++) {
+    print('${grades[i].subjectName}: ${grades[i].grade}');
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)  => Splash();
+  Widget build(BuildContext context) => Splash();
 }
