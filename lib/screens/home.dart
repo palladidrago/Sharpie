578eeb0de133438984d0f71f89197f7dd9981ed5
David Ohayon <davideytanohayon@gmail.com>
2021-01-10 20:27:43 +0200

    Code clean up

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -203,28 +203,29 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-/* class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Sharpie",
-      theme: ThemeData(
-        fontFamily: "Josefin",
-        scaffoldBackgroundColor: Colors.red[300],
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Grades'),
-        ),
-        body: SafeArea(
-          child: Stack(
-            children: <Widget>[
-              GradeList(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-} */
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "Sharpie",
+//       theme: ThemeData(
+//         fontFamily: "Josefin",
+//         scaffoldBackgroundColor: Colors.red[300],
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Grades'),
+//         ),
+//         body: SafeArea(
+//           child: Stack(
+//             children: <Widget>[
+//               GradeList(),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
